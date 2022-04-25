1c4a9cc333251792afc986c32b712200bbc0430a
marti <marti@DESKTOP-IQDB4G0>
2022-04-26 01:38:11 +0300

    Add project files.

---
+++ Employee_management_system/main.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Employee_management_system
+{
+    partial class main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.EmployeeIdlbl = new System.Windows.Forms.Label();
+            this.Newbtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.EmployeeNamelbl = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Addresslbl = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Salarylbl = new System.Windows.Forms.Label();
+            this.Rolelbl = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Insertbtn = new System.Windows.Forms.Button();
+            this.Updatebtn = new System.Windows.Forms.Button();
+            this.Deletebtn = new System.Windows.Forms.Button();
+            this.Clearbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EmployeeIdlbl
+            // 
+            this.EmployeeIdlbl.AutoSize = true;
+            this.EmployeeIdlbl.Location = new System.Drawing.Point(82, 50);
+            this.EmployeeIdlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.EmployeeIdlbl.Name = "EmployeeIdlbl";
+            this.EmployeeIdlbl.Size = new System.Drawing.Size(101, 21);
+            this.EmployeeIdlbl.TabIndex = 0;
+            this.EmployeeIdlbl.Text = "Employee Id";
+            // 
+            // Newbtn
+            // 
+            this.Newbtn.Location = new System.Drawing.Point(55, 533);
+            this.Newbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Newbtn.Name = "Newbtn";
+            this.Newbtn.Size = new System.Drawing.Size(138, 41);
+            this.Newbtn.TabIndex = 1;
+            this.Newbtn.Text = "New";
+            this.Newbtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(219, 47);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(219, 99);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(253, 29);
+            this.textBox2.TabIndex = 4;
+            // 
+            // EmployeeNamelbl
+            // 
+            this.EmployeeNamelbl.AutoSize = true;
+            this.EmployeeNamelbl.Location = new System.Drawing.Point(82, 102);
+            this.EmployeeNamelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.EmployeeNamelbl.Name = "EmployeeNamelbl";
+            this.EmployeeNamelbl.Size = new System.Drawing.Size(129, 21);
+            this.EmployeeNamelbl.TabIndex = 3;
+            this.EmployeeNamelbl.Text = "Employee Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(219, 154);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(253, 29);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Addresslbl
+            // 
+            this.Addresslbl.AutoSize = true;
+            this.Addresslbl.Location = new System.Drawing.Point(82, 157);
+            this.Addresslbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Addresslbl.Name = "Addresslbl";
+            this.Addresslbl.Size = new System.Drawing.Size(70, 21);
+            this.Addresslbl.TabIndex = 5;
+            this.Addresslbl.Text = "Address";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(219, 210);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(253, 29);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Salarylbl
+            // 
+            this.Salarylbl.AutoSize = true;
+            this.Salarylbl.Location = new System.Drawing.Point(82, 213);
+            this.Salarylbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Salarylbl.Name = "Salarylbl";
+            this.Salarylbl.Size = new System.Drawing.Size(54, 21);
+            this.Salarylbl.TabIndex = 7;
+            this.Salarylbl.Text = "Salary";
+            // 
+            // Rolelbl
+            // 
+            this.Rolelbl.AutoSize = true;
+            this.Rolelbl.Location = new System.Drawing.Point(82, 270);
+            this.Rolelbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Rolelbl.Name = "Rolelbl";
+            this.Rolelbl.Size = new System.Drawing.Size(43, 21);
+            this.Rolelbl.TabIndex = 9;
+            this.Rolelbl.Text = "Role";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Clerk",
+            "Manager",
+            "CEO"});
+            this.comboBox1.Location = new System.Drawing.Point(219, 267);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(253, 29);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // Insertbtn
+            // 
+            this.Insertbtn.Location = new System.Drawing.Point(248, 533);
+            this.Insertbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Insertbtn.Name = "Insertbtn";
+            this.Insertbtn.Size = new System.Drawing.Size(138, 41);
+            this.Insertbtn.TabIndex = 11;
+            this.Insertbtn.Text = "Insert";
+            this.Insertbtn.UseVisualStyleBackColor = true;
+            // 
+            // Updatebtn
+            // 
+            this.Updatebtn.Location = new System.Drawing.Point(450, 533);
+            this.Updatebtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Updatebtn.Name = "Updatebtn";
+            this.Updatebtn.Size = new System.Drawing.Size(138, 41);
+            this.Updatebtn.TabIndex = 12;
+            this.Updatebtn.Text = "Update";
+            this.Updatebtn.UseVisualStyleBackColor = true;
+            // 
+            // Deletebtn
+            // 
+            this.Deletebtn.Location = new System.Drawing.Point(641, 533);
+            this.Deletebtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Deletebtn.Name = "Deletebtn";
+            this.Deletebtn.Size = new System.Drawing.Size(138, 41);
+            this.Deletebtn.TabIndex = 13;
+            this.Deletebtn.Text = "Delete";
+            this.Deletebtn.UseVisualStyleBackColor = true;
+            // 
+            // Clearbtn
+            // 
+            this.Clearbtn.Location = new System.Drawing.Point(826, 533);
+            this.Clearbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.Clearbtn.Name = "Clearbtn";
+            this.Clearbtn.Size = new System.Drawing.Size(138, 41);
+            this.Clearbtn.TabIndex = 14;
+            this.Clearbtn.Text = "Clear";
+            this.Clearbtn.UseVisualStyleBackColor = true;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 630);
+            this.Controls.Add(this.Clearbtn);
+            this.Controls.Add(this.Deletebtn);
+            this.Controls.Add(this.Updatebtn);
+            this.Controls.Add(this.Insertbtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Rolelbl);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Salarylbl);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Addresslbl);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.EmployeeNamelbl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Newbtn);
+            this.Controls.Add(this.EmployeeIdlbl);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label EmployeeIdlbl;
+        private Button Newbtn;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label EmployeeNamelbl;
+        private TextBox textBox3;
+        private Label Addresslbl;
+        private TextBox textBox4;
+        private Label Salarylbl;
+        private Label Rolelbl;
+        private ComboBox comboBox1;
+        private Button Insertbtn;
+        private Button Updatebtn;
+        private Button Deletebtn;
+        private Button Clearbtn;
+    }
+}
